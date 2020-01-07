2a4ed3a941124afc1cf80c76d475aaef262045cb
Esther Walker <equestriennesrule@yahoo.com>
2020-01-06 20:30:20 -0800

    able to make a date call to api to get user input

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 var userInput = document.querySelector('#userinput');
 var searchButton = document.querySelector('#searchbutton');
 
+
 searchButton.addEventListener('click', function () {
-
-    console.log(userinput.value);
+    var city = userInput.value;
+    populateWeatherdata(city);
 })
 
-var queryUrl;
 
-queryUrl = "https//api.openweathermap.org/data/2.5/weather?q=Seattle,us&APPID=9539fd31c5b05c00c30c1fd1ee767fdc";
 
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-})
+
+
+
+
+
+
+
+
+
+function populateWeatherdata(city) {
+
+
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=9539fd31c5b05c00c30c1fd1ee767fdc";
+
+    $.ajax({
+        url: queryUrl,
+        method: 'GET'
+    })
+        .done(function (result) {
+            console.log(result);
+        })
+}
+
+
+
