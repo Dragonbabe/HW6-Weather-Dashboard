fe5fe6e9395aab6128edb67abe1c1b591d604072
Esther Walker <equestriennesrule@yahoo.com>
2020-01-06 20:02:09 -0800

    Added a call to the open weather API

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ searchButton.addEventListener('click', function () {
     console.log(userinput.value);
 })
 
-
-
-
-
-
 var queryUrl;
+
 queryUrl = "https//api.openweathermap.org/data/2.5/weather?q=Seattle,us&APPID=9539fd31c5b05c00c30c1fd1ee767fdc";
+
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+})
