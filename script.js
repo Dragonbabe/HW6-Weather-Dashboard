ece5411d227aff58d5b3a5a4570f4c914b3f1ee7
Esther Walker <equestriennesrule@yahoo.com>
2020-01-07 18:25:13 -0800

    Added display id's in html and made them functional in js

--- script.js
+++ script.js
@@ -5,14 +5,10 @@ var searchButton = document.querySelector('#searchbutton');
 searchButton.addEventListener('click', function () {
     var city = userInput.value;
     populateWeatherdata(city);
+    console.log(userInput);
+
+
 })
-
-
-
-
-
-
-
 
 
 
@@ -21,16 +17,29 @@ searchButton.addEventListener('click', function () {
 function populateWeatherdata(city) {
 
 
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=9539fd31c5b05c00c30c1fd1ee767fdc";
+    //var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=9539fd31c5b05c00c30c1fd1ee767fdc";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather";
 
     $.ajax({
         url: queryUrl,
-        method: 'GET'
-    })
-        .done(function (result) {
-            console.log(result);
-        })
-}
+        data: {
+            q: city,
+            units: "imperial",
+            APPID: "9539fd31c5b05c00c30c1fd1ee767fdc"
 
+        },
+        method: 'GET'
+    }).done(function (result) {
+        console.log(result);
+        console.log(result.main.temp);
+        //document.querySelector('#displaytemp').value = result.main.temp;
+        $("#displaytemp").html(result.main.temp);
+        $("#displayhumidity").html(result.main.humidity);
+        $("#displaywindspeed").html(result.wind.speed);
+
+
+    })
+
+}
 
 
