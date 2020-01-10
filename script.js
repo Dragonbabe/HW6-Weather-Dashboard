d68570d1f38cd59e0b973107f4d8aa6e4ff2eb41
Esther Walker <equestriennesrule@yahoo.com>
2020-01-09 18:26:01 -0800

    added icons, worked on columns to display

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ $(document).ready(function () {
     function populateWeatherdata(city) {
 
         var queryUrl = "https://api.openweathermap.org/data/2.5/weather";
+        var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast";
         var lat;
         var lon;
 
@@ -28,12 +29,45 @@ $(document).ready(function () {
             },
             method: 'GET'
         }).then(function (result) {
+            console.log(result.weather[0].icon);
+            let imgURL = './assets/' + result.weather[0].icon + '@2x.png';
+            console.log(imgURL);
 
+            $('#icons').attr("src", imgURL);
             $("#displaytemp").text(result.main.temp);
             $("#displayhumidity").text(result.main.humidity);
             $("#displaywindspeed").text(result.wind.speed);
             getUVIndex(result.coord.lat, result.coord.lon);
+            $("#currentcity").text(result.name);
         });
+
+        $.ajax({
+            url: forecastUrl,
+            data: {
+                q: city,
+                units: "imperial",
+                APPID: "9539fd31c5b05c00c30c1fd1ee767fdc"
+
+            },
+            method: 'GET'
+        }).then(function (result) {
+            console.log(result);
+
+            // let imgURL = './assets/' + result.weather[0].icon + '@2x.png';
+
+            // console.log(imgURL);
+            $("#display5dayforecast").text(result.list[0].main.temp);
+            // $('#icons').attr("src", imgURL);
+            // $("#displaytemp").text(result.main.temp);
+            // $("#displayhumidity").text(result.main.humidity);
+            // $("#displaywindspeed").text(result.wind.speed);
+            // getUVIndex(result.coord.lat, result.coord.lon);
+            //let html = '<h2>' + cityname + '</h>' + 
+
+            //$('your new div name ').append(html)
+
+        });
+
     }
     function getUVIndex(lat, lon) {
 
